7902d61d6bdf52451536ed8adee20d3de697d71a
Rukul-Learner <78303638+Rukul-Learner@users.noreply.github.com>
2022-06-11 14:06:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,32 +12,45 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(express.static("public"));
 
 
 
-app.get("/", function (req, res) {
-    res.render("home", { startingContent : homeStartingContent })
+app.get("/", function(req, res) {
+    res.render("home", {
+        startingContent: homeStartingContent
+    })
 });
 
-app.get("/about", function (req, res) {
-    res.render("about", { aboutContent : aboutContent })
+app.get("/about", function(req, res) {
+    res.render("about", {
+        aboutContent: aboutContent
+    })
 });
 
-app.get("/contact", function (req, res) {
-    res.render("contact", { contactContent : contactContent })
+app.get("/contact", function(req, res) {
+    res.render("contact", {
+        contactContent: contactContent
+    })
 });
 
-app.get("/compose", function (req, res) {
+app.get("/compose", function(req, res) {
     res.render("compose");
 });
 
-app.post("/compose", function (req, res) {
-    console.log(req.body.postTitle);
+app.post("/compose", function(req, res) {
+    const post = {
+        title: req.body.postTitle,
+        content: req.body.postBody
+    };
+
+    console.log(post);
     res.redirect("/compose");
 });
 
 app.listen(3000, function() {
-  console.log("Server started on port 3000");
+    console.log("Server started on port 3000");
 });
